480b520c04e7d75c6f4776eeedf27d3497b66149
hyunnn0105 <asha010519@gmail.com>
2022-05-01 23:25:10 +0900

    220501 함수복습

--- 1.core-js/8.함수/5z2.function_quiz02.js
+++ 1.core-js/8.함수/5z2.function_quiz02.js
@@ -1,30 +1,33 @@
+/*
+    n개의 정수를 전달받아 그 중 최소값을
+    반환하는 함수 min을 정의하고 
 
-function min (numbers) {
+    사용자에게 반복해서 숫자들을 입력받은 후
+    min을 호출하여 최소값을 출력하세요.
+    입력을 그만둘때는 '그만'이라고 입력하도록
+    코드를 작성하세요
+*/
+
+function min(numbers) {
     var minNum = numbers[0];
     for (var n of numbers) {
-        if (n<minNum){
+        if (n < minNum) {
             minNum = n;
         }
     }
     return minNum;
 }
 
-var nums =[];
+var nums = [];
 
 while (true) {
-    var inputMS = prompt('숫자를 입력 : ');
-    if (inputMS === '그만') break;
-    nums.push(+inputMS);
+    var inputMsg = prompt('숫자를 입력: ');
+    if (inputMsg === '그만') break;
+    nums.push(+inputMsg);
 }
 
 var result = min(nums);
-console.log(`${result}`);
-
-
-
-
-
-
+alert(result);
 
 
 /*
--- 1.core-js/8.함수/5z4.function_quiz04.js
+++ 1.core-js/8.함수/5z4.function_quiz04.js
@@ -7,6 +7,30 @@
     작성하고 아래 그림과 같이 출력하세요.
 */
 
+
+function findNumberCount(arr, targetNum) {
+    var idxArr = []; //인덱스위치들을 저장할 배열
+    for (var i=0; i< arr.length; i++) {
+        if (arr[i] === targetNum) {
+            idxArr.push(i);
+        }
+    }
+    return idxArr;
+}
+
+// 배열의 개수를 알려주는 함수
+function count(arr) {
+    return arr.length;
+}
+
+var numbers = [9, 9, 10, 33, 9, 44, 9, 44, 11, 9];
+
+var idxArray = findNumberCount(numbers, 9);
+console.log(idxArray);
+console.log(`타겟의 개수: ${count(idxArray)}`);
+
+
+
 // function search (arr, find) {
 //     var findNum = vx.includes(find);
 //     var iNum = vx.indexOf(findNum);
@@ -27,22 +51,22 @@
 // console.log(`${find}는 ${i}번째에 있습니다.`);
 
 
-function search(arr, targetNum) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === targetNum) {
-            return i;
-        }
-    }
-    return -1;
-}
+// function search(arr, targetNum) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] === targetNum) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
 
-function search2(arr, targetNum) {
-    return arr.indexOf(targetNum);
-}
+// function search2(arr, targetNum) {
+//     return arr.indexOf(targetNum);
+// }
 
-//==========================
+// //==========================
 
-var numbers = [99, 33, 44, 100, 3, 67];
+// var numbers = [99, 33, 44, 100, 3, 67];
 
-var result = search(numbers, 3);
-console.log(`result: ${result}`);
+// var result = search(numbers, 3);
+// console.log(`result: ${result}`);
